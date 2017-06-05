d1dcc47e0497de60a6d2141af4ece1969c27638f
Zidane Abubakar <Zii@Zidanes-MacBook-Pro.local>
2017-06-05 13:13:31 -0400

    Commit flowable

--- src/shoobx/rml2docx/flowable.py
+++ src/shoobx/rml2docx/flowable.py
@@ -31,6 +31,7 @@ class Paragraph(directive.RMLDirective):
         return text
 
     def _handleText(self, element, paragraph):
+        # Maybe recursive implementation for nested tags
         if element.text is not None and element.text.strip() != '':
             run = paragraph.add_run(self._cleanText(element.text.lstrip()))
 
@@ -58,12 +59,80 @@ class Paragraph(directive.RMLDirective):
         self._handleText(self.element, paragraph)
         return paragraph
 
+class Title(directive.RMLDirective):
+    signature = rml_flowable.IParagraph
+    defaultStyle = None
+
+    def _cleanText(self, text):
+        if not text:
+            text = ''
+        text = re.sub('\n\s+', ' ', text)
+        text = re.sub('\s\s\s+', '', text)
+        text = re.sub('\t', '', text)
+        return text
+
+    def _handleText(self, element, title):
+        # Maybe recursive implementation for nested tags
+        if element.text is not None and element.text.strip() != '':
+            run = title.add_run(self._cleanText(element.text.lstrip()))
+            run.bold = True
+            run.underline = True
+
+    def process(self):
+        style = self.element.attrib.get('style', self.defaultStyle)
+        title = self.parent.container.add_paragraph(style=style)
+        self._handleText(self.element, title)
+        return title
+
+class Header(directive.RMLDirective):
+    signature = rml_flowable.IParagraph
+    defaultStyle = None
+
+    def _cleanText(self, text):
+        if not text:
+            text = ''
+        text = re.sub('\n\s+', ' ', text)
+        text = re.sub('\s\s\s+', '', text)
+        text = re.sub('\t', '', text)
+        return text
+
+    def _handleText(self, element, header):
+        # Maybe recursive implementation for nested tags
+        if element.text is not None and element.text.strip() != '':
+            run = header.add_run(self._cleanText(element.text.lstrip()))
+
+        if element.tag[-1] == '1':
+            run.bold = True
+        elif element.tag[-1] == '2':
+            run.italic = True
+        elif element.tag[-1] == '3':
+            run.underline = True
+        elif element.tag[-1] == '4':
+            run.bold = True
+        elif element.tag[-1] == '5':
+            run.italic = True
+        elif element.tag[-1] == '6':
+            run.underline = True
+
+    def process(self):
+        style = self.element.attrib.get('style', self.defaultStyle)
+        header = self.parent.container.add_paragraph(style=style)
+        self._handleText(self.element, header)
+        return header
 
 class Flow(directive.RMLDirective):
-
     factories = {
         # Paragraph-Like Flowables
         'para': Paragraph,
+        # Text which should appear in a title
+        'Title': Title,
+        # Headers
+        'h1': Header,
+        'h2': Header,
+        'h3': Header,
+        'h4': Header,
+        'h5': Header,
+        'h6': Header
     }
 
     def __init__(self, *args, **kw):
@@ -71,3 +140,5 @@ class Flow(directive.RMLDirective):
 
     def process(self):
         self.processSubDirectives()
+
+

6ed9223b81d52965953d973ccac0279b1c3fb354
Zidane Abubakar <Zii@Zidanes-MacBook-Pro.local>
2017-06-05 13:14:58 -0400

    commit flowable

--- src/shoobx/rml2docx/flowable.py
+++ src/shoobx/rml2docx/flowable.py
@@ -130,7 +130,7 @@ class Flow(directive.RMLDirective):
         'h1': Header,
         'h2': Header,
         'h3': Header,
-        'h4': Header,
+        'h4': Header, 
         'h5': Header,
         'h6': Header
     }
